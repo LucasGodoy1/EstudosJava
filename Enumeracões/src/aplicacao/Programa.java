e3f0f7d50a7b0e1cb7d5a6762e733fd95a60de8e
Lucas Godoy <lucassdegodoy@hotmail.com>
2024-02-10 14:26:31 -0300

    testando sincronização

--- Enumeracões/src/aplicacao/Programa.java
+++ Enumeracões/src/aplicacao/Programa.java
@@ -10,6 +10,7 @@ public class Programa {
 	public static void main(String[] args) {
 		Ordem ordem = new Ordem(1080, new Date(), OrdemDeStatus.PAGAMENTO_PENDENTE);
 		System.out.println(ordem);
+		
 		OrdemDeStatus os1 = OrdemDeStatus.ENTREGUE;
 		System.out.println(os1);
 
