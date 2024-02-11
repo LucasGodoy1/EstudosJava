683de7bc133a2703e8b500b30e6b4decad6b25a9
Lucas Godoy <lucassdegodoy@hotmail.com>
2024-02-11 18:46:52 -0300

    adicionado construtores e simplificando o codigo na main

--- Triangulo/src/aplicacao/App.java
+++ Triangulo/src/aplicacao/App.java
@@ -7,28 +7,22 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Triangulo triangulo1 = new Triangulo();
-        Triangulo triangulo2 = new Triangulo();
-
-
         System.out.println("Entre com os lados do triangulo1");
-        triangulo1.a = sc.nextDouble();
-        triangulo1.b = sc.nextDouble();
-        triangulo1.c = sc.nextDouble();
+        Triangulo triangulo1 = new Triangulo(sc.nextDouble(), sc.nextDouble(), sc.nextDouble());
         System.out.println("Entre com os lados do triangulo2");
-        triangulo2.a = sc.nextDouble();
-        triangulo2.b = sc.nextDouble();
-        triangulo2.c = sc.nextDouble();
+        Triangulo triangulo2 = new Triangulo(sc.nextDouble(), sc.nextDouble(), sc.nextDouble());
+
+        sc.close();
 
         System.out.printf("Area Triangulo 1: %.2f\n", triangulo1.calculaArea());
 
         System.out.printf("Area Triangulo 2: %.2f\n", triangulo2.calculaArea());
         if (triangulo2.calculaArea() > triangulo1.calculaArea()){
-            System.out.println(String.format("A maior area é trianguolo 2 - > %.2f", triangulo2.calculaArea()));
+            System.out.println(String.format("A maior area trianguolo 2 - > %.2f", triangulo2.calculaArea()));
         }else if (triangulo1.calculaArea() == triangulo2.calculaArea()){
             System.out.println(String.format("Os dois triangulos tem o mesmo tamanho -> %.2f", triangulo1.calculaArea()));
         }else{
-            System.out.println(String.format("A maior area é trianguolo 1 -> %.2f", triangulo1.calculaArea()));
+            System.out.println(String.format("A maior area trianguolo 1 -> %.2f", triangulo1.calculaArea()));
         }
 
 
--- Triangulo/src/entidade/Triangulo.java
+++ Triangulo/src/entidade/Triangulo.java
@@ -5,6 +5,15 @@ public class Triangulo {
     public double b;
     public double c;
 
+    public Triangulo() {
+    }
+
+    public Triangulo(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
     public Double calculaArea(){
        Double t = (this.a + this.b + this.c) /2.0;
        return Math.sqrt(t * (t - this.a) * (t - this.b) * (t - this.c));
