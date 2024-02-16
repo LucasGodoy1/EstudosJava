4a33e385029a9febf0238bdf3355ce378e7420f0
Lucas Godoy <lucassdegodoy@hotmail.com>
2024-02-16 19:53:53 -0300

    teste de push

---
+++ exercicios-jpa/src/Teste.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Teste {
+
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql://localhost";
+        String usuario = "root";
+        String senha = "961411";
+
+        Connection conexao = DriverManager.
+                getConnection(url,usuario,senha);
+
+        System.out.println("conexao executada com sucesso!");
+
+        conexao.close();
+    }
+}
